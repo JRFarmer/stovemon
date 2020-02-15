1c34d6426dcfb94d204b79fb12e9c0dd5cbd934b
Jonathan Farmer <jrfarmer.com@gmail.com>
2020-02-14 21:09:34 -0500

    ignore sporadic shorts

--- stovemon.py
+++ stovemon.py
@@ -32,7 +32,11 @@ def main(cfgfile):
     aio = Adafruit_IO.Client(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
 
     # setup the thermocouple connection
-    thermo = MAX31855(board.SPI(), SPIDEV_DEVICE)
+    spi = board.SPI()
+    while not spi.try_lock(): pass
+    spi.configure(baudrate=1000000)
+    spi.unlock()
+    thermo = MAX31855(spi, SPIDEV_DEVICE)
 
     # setup the display
     i2c = board.I2C()
@@ -43,10 +47,20 @@ def main(cfgfile):
     display.fill(0)
     display.print("----")
 
-    count = 0
+    count = -1
     while (True):
 
-        temp = ctof(thermo.temperature)
+        count = count + 1
+        time.sleep(1)
+
+        try:
+            temp = ctof(thermo.temperature)
+            ref = ctof(thermo.reference_temperature)
+        except RuntimeError as e:
+            if "short" in str(e):
+                print(str(e))
+                continue
+            raise
 
         if (count % DISPLAY_PERIOD == 0):
             # update the display
@@ -59,14 +73,9 @@ def main(cfgfile):
             aio.send_data(temp_feed.key, temp)
 
         if (count % CJ_PERIOD == 0):
-            ref = temp = ctof(thermo.reference_temperature)
-
             # push cold junction temperature
             cj_feed = aio.feeds(COLDJUNCTION_FEED)
             aio.send_data(cj_feed.key, ref)
-
-        count = count + 1
-        time.sleep(1)
 
 if __name__ == "__main__":
     main(sys.argv[1])
